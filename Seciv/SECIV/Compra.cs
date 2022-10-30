d0f2a6ebd08de7c3116fe5ffd72c6d49a20c5296
Ronald Blanco <ronald.blanco.navarro@est.una.ac.cr>
2022-10-30 11:15:12 -0600

    Prueba Basica WPF MongDB

---
+++ Seciv/SECIV/Compra.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECIV
+{
+    internal class Compra
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        
+        [BsonElement("Cod_Compra")]
+        public String com_codigo { get; set; }        
+        
+        [BsonElement("Monto_Compra")]
+        public Double com_monto { get; set; }
+
+        [BsonElement("Fecha_Compra")]
+        public String com_fecha { get; set; }
+
+        public Compra(string com_codigo, double com_monto, String com_fecha)
+        {
+            this.com_codigo = com_codigo;
+            this.com_monto = com_monto;
+            this.com_fecha = com_fecha;
+        }
+    }
+}
---
+++ Seciv/SECIV/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SECIV
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static MongoClient client = new MongoClient("mongodb+srv://sys:sys@inge01.actyoaw.mongodb.net/SECIV?retryWrites=true&w=majority"); // new MongoClient();
+        static IMongoDatabase db = client.GetDatabase("SECIV");
+        static IMongoCollection<Compra> collection = db.GetCollection<Compra>("Compras");
+        
+        public void ReadAllCompras()
+        {
+            List<Compra> list = collection.AsQueryable().ToList<Compra>();
+            dgridCompras.ItemsSource = list;
+            Compra c = (Compra)dgridCompras.Items.GetItemAt(0);
+            txtId.Text = c.Id.ToString();
+            txtCodigo.Text = c.com_codigo.ToString();
+            txtMonto.Text = c.com_monto.ToString();
+            txtFecha.Text = c.com_fecha.ToString();
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadAllCompras();
+        }
+
+        private void dgridCompras_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Compra c = (Compra)dgridCompras.SelectedItem;
+            txtId.Text = c.Id.ToString();
+            txtCodigo.Text = c.com_codigo.ToString();
+            txtMonto.Text = c.com_monto.ToString();
+            txtFecha.Text = c.com_fecha.ToString();
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            Compra c = new Compra(txtCodigo.Text, Double.Parse(txtMonto.Text), txtFecha.Text);
+            collection.InsertOne(c);
+            ReadAllCompras();
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+            var updateDef = Builders<Compra>.Update.Set("com_codigo", txtCodigo.Text).Set("com_monto", Double.Parse(txtMonto.Text)).Set("com_fecha", txtFecha.Text);
+            collection.UpdateOne(c => c.Id == ObjectId.Parse(txtId.Text), updateDef);
+            ReadAllCompras();
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            collection.DeleteOne(b => b.Id == ObjectId.Parse(txtId.Text));
+            ReadAllCompras();
+        }
+    }
+}
