47ed9580aac7f35b4bda3734276a56803e5b6364
Ronald Blanco <ronald.blanco.navarro@est.una.ac.cr>
2022-10-30 17:00:27 -0600

    agarrando figura

--- Seciv/CapaLogica/LogicaNegocio/Compra.cs
+++ Seciv/CapaLogica/LogicaNegocio/Compra.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace CapaLogica.LogicaNegocio
 {
     internal class Compra
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("Cod_Compra")]
+        public string Cod_Compra { get; set; }
+
+        [BsonElement("Monto_Compra")]
+        public float Monto_Compra { get; set; }
+
+        [BsonElement("Fecha_Compra")]
+        public string Fecha_Compra { get; set; }
     }
 }
--- Seciv/CapaLogica/Servicios/ServicioCompra.cs
+++ Seciv/CapaLogica/Servicios/ServicioCompra.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using CapaLogica.LogicaNegocio;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CapaLogica.Servicios
 {
     internal class ServicioCompra
     {
+
+        
+
     }
 }
