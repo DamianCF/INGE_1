6695b3fae5167aa92f1828d8ac222ffdce4c169d
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2022-11-10 13:56:55 -0600

    CierreCajas, Gestor y Servicio

---
+++ Seciv/SECIV/CapaLogica/LogicaNegocio/CierreCaja.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica.LogicaNegocio
+{
+    public class CierreCaja
+    {
+        public CierreCaja(Double cc_cierreCompras, Double cc_cierreVentas, Double cc_saldoFinal, string cc_estado, string cc_fehca)
+        {
+            this.id = id;
+            CierreCompras = cc_cierreCompras;
+            CierreVentas = cc_cierreVentas;
+            SaldoFinal = cc_saldoFinal;
+            Estado = cc_estado;
+            Fecha = cc_fehca;
+
+        }
+
+        public CierreCaja(string id, Double cc_cierreCompras, Double cc_cierreVentas, Double cc_saldoFinal, string cc_estado, string cc_fehca)
+        {
+            CierreCompras = cc_cierreCompras;
+            CierreVentas = cc_cierreVentas;
+            SaldoFinal = cc_saldoFinal;
+            Estado = cc_estado;
+            Fecha = cc_fehca;
+
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("CierreCompras")]
+        public Double CierreCompras { get; set; }
+
+        [BsonElement("CierreVentas")]
+        public Double CierreVentas { get; set; }
+
+        [BsonElement("SaldoFinal")]
+        public Double SaldoFinal { get; set; }
+
+        [BsonElement("Estado")]
+        public string Estado { get; set; }
+
+        [BsonElement("Fecha")]
+        public string Fecha { get; set; }
+    }
+}
---
+++ Seciv/SECIV/CapaLogica/Servicios/ServicioCierreCaja.cs
@@ -0,0 +1,44 @@
+﻿using CapaConexion;
+using CapaLogica.LogicaNegocio;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica.Servicios
+{
+    public class ServicioCierreCaja: IDisposable
+    {
+        public ServicioCierreCaja()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+
+        static IMongoCollection<CierreCaja> collection = conexion.getDataBase().GetCollection<CierreCaja>("CierreCaja");
+
+        public void InsertarCierreCaja(CierreCaja c)
+        {
+            collection.InsertOne(c);
+        }
+        public List<CierreCaja> ListarCierreCaja()
+        {
+            return collection.AsQueryable().ToList<CierreCaja>(); // collection.Find(x => true).ToList(); 
+        }
+        public void ActualizarCierreCaja(CierreCaja c)
+        {
+            collection.ReplaceOne(x => x.id == c.id, c);
+        }
+        public void EliminarCierreCaja(string id)
+        {
+            collection.DeleteOne(x => x.id == id);
+        }
+
+
+    }
+}
