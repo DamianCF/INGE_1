9b0ccb8b670460f25fd009e864c11ceb758807c9
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2022-11-16 19:25:40 -0600

    Inventario

--- Seciv/SECIV/CapaPresentacion/MainWindow.xaml.cs
+++ Seciv/SECIV/CapaPresentacion/MainWindow.xaml.cs
@@ -36,5 +36,10 @@ namespace CapaPresentacion
         {
             Grid.Content = new Modulo_Contabilidad.mainContabilidadView();
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Grid.Content = new Modulo_Inventarios.mainInventarios();
+        }
     }  
 }
---
+++ Seciv/SECIV/CapaPresentacion/Modulo_Inventarios/mainInventarios.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapaPresentacion.Modulo_Inventarios
+{
+    /// <summary>
+    /// Lógica de interacción para mainInventarios.xaml
+    /// </summary>
+    public partial class mainInventarios : Page
+    {
+        public mainInventarios()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
